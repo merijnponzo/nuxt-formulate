5ca24d44af144c4d4b9c4f02194d7461a6276513
merren <merijn@ponzo.nl>
2021-06-26 10:05:44 +0200

    push
    push

---
+++ components/Logo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <FormulateForm v-model="values" :schema="schema" />
+    {{ values }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormFieldsGen",
+
+  data() {
+    return {
+      formCopySchema: [],
+      values: {},
+      isLoading: false,
+      schema: [
+        {
+          type: "group",
+          repeatable: true,
+          name: "addresses",
+          addLabel: "+ Address",
+          children: [
+            {
+              name: "street",
+              label: "Street address"
+            },
+            {
+              name: "city",
+              label: "City"
+            },
+            {
+              component: "div",
+              class: "double-row",
+              children: [
+                {
+                  name: "state",
+                  type: "select",
+                  label: "State",
+                  options: {
+                    va: "Virginia",
+                    fl: "Florida",
+                    ne: "Nebraska",
+                    ca: "California"
+                  }
+                },
+                {
+                  name: "zip",
+                  label: "Zip"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.formCopySchema = [...this.schema];
+  },
+  methods: {
+    submitHandler(data) {
+      console.log(data);
+    }
+  }
+};
+</script>
